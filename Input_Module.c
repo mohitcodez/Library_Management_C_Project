c84ba2d995dbd2b5e4ee6f47001ee1fd2b41c5cc
mohitcodez <mohitthakur0130@gmail.com>
2025-07-09 21:06:03 +0530

    add choice loop

--- Input_Module.c
+++ Input_Module.c
@@ -9,17 +9,21 @@ struct info{
 }b;
 int main(){
   char ch;
-  
+  printf("do you want to input book Y/y");
+  scanf("%c",&ch);
 while(ch=='Y'|| ch=='y'){
   printf("do you want to input book Y/y");
   scanf("%c",&ch);
-  printf("enter book name");
+  printf("enter book name:");
   scanf("%s",&b.book_name);
   getchar(b.book_name);
-
+  printf("enter author name:");
+  scanf("%s",b.author);
+  getchar(b.author);
   
 }
-
+printf("do you want to input book Y/y");
+  scanf("%c",&ch);
 
 
 return 0;}
