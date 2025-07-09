f1b68537770d14fa935705bf2e99dde504f209c2
mohitcodez <mohitthakur0130@gmail.com>
2025-07-09 20:56:02 +0530

    add changes in input module

--- Input_Module.c
+++ Input_Module.c
@@ -1,7 +1,25 @@
 struct info{
+
+  
   char book_name[20];
   char author[25];
   int price;
   char genre[10];
   int stock;
 }b;
+int main(){
+  char ch;
+  
+while(ch=='Y'|| ch=='y'){
+  printf("do you want to input book Y/y");
+  scanf("%c",&ch);
+  printf("enter book name");
+  scanf("%s",&b.book_name);
+  getchar(b.book_name);
+
+  
+}
+
+
+
+return 0;}
