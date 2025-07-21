2c4b8560a6dcff9d75c28405876cf3183fe82620
mohitcodez <mohitthakur0130@gmail.com>
2025-07-22 01:45:12 +0530

    added search module

--- main.c
+++ main.c
@@ -2,22 +2,28 @@
 
 void add_book();
 void readbook( );
+void callsearch();
 void main(){
     int m=0;
-    while(m!=4){
+    while(m!=5){
     printf("----------------------\n");
     printf("1. Add Book\n");
     printf("2. Veiw Book\n");
     printf("3. Delete Book\n");
-    printf("4. exit\n");
+    printf("4. Search Book\n");
+    printf("5. exit\n");
     scanf("%d",&m);
     switch(m){
         case 1:add_book();
         break;
         case 2:readbook( );
         break;
-        case 4:printf("program exited");
+        case 4: callsearch();
         break;
+        case 5:printf("program exited");
+
+        break;
+        
         default:printf("out of range command");
 
 
---
+++ search.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<string.h>
+struct info{
+
+  
+  char book_name[20];
+  char author[25];
+  int price;
+  char genre[15];
+  int stock;
+}b;
+
+
+void search(struct info b, char sn[20]){
+FILE*pp;
+int n=0;
+pp=fopen("input.txt","r");
+if(pp == NULL){
+    return ;
+}
+
+    printf("\n--- Book Records ---\n");
+
+while (fscanf(pp, "%s", b.book_name) != EOF) {
+    if (strcmp(sn,b.book_name)==0){
+      fscanf(pp, "%s", b.author);
+    fscanf(pp, "%d", &b.price);
+    fscanf(pp, "%s", b.genre);
+    fscanf(pp, "%d", &b.stock);
+
+
+        printf("\nBook Name: %s\n", b.book_name);
+        printf("Author: %s\n", b.author);
+        printf("Price: %d\n", b.price);
+        printf("Genre: %s\n", b.genre);
+        printf("Stock: %d\n", b.stock);
+        n=n+1;
+break;
+
+    }
+    
+
+
+
+
+}
+if(n==0){
+        printf(" no book found\n");
+    }
+fclose(pp);
+}
+void callsearch(){
+
+    char sn[20];
+    getchar();
+    printf("enter book name:");
+    gets(sn);
+    
+    search(b,sn);
+
+}
